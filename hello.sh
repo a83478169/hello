8c3a4850c478b98060528c9db62b49f34b0d6731
a83478169 <83478169@qq.com>
2021-11-08 13:03:09 +0800

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,41 @@
+filepath1=/root/ql/config/env.sh
+filepath2=/root/ql1/config/env.sh
+filepath3=/root/ql2/config/env.sh
+echo "`date +%Y%m%d` start upload......"
+echo "`date +%Y%m%d` start upload......" > /home/import.log
+
+sleep 1
+
+password='Xiao1992'
+
+echo "start copy datafile from remote path..."
+echo "start copy datafile from remote path..." >> /home/import.log
+
+/usr/bin/expect <<EOF
+set timeout 10
+spawn scp -r -p $filepath1 root@1.15.15.178:/root/env1`date +%x%X`.sh
+spawn scp -r -p $filepath2 root@1.15.15.178:/root/env2`date +%x%X`.sh
+spawn scp -r -p $filepath3 root@1.15.15.178:/root/env3`date +%x%X`.sh
+expect {
+ "(yes/no)?"
+ {
+   send "yes\n"
+   expect "*assword:" {send "$password\n"}
+ }
+ "*assword:"
+ {
+   send "$password\n"
+ }
+}
+send "exit\n"
+expect eof
+EOF
+
+if [ $? = 0 ]; then
+
+        echo "datafile copy successful!"
+        echo "datafile copy successful!" >> /home/import.log
+else
+        echo "datafile copy failed!"
+        echo "datafile copy failed!" >> /home/import.log
+fi
